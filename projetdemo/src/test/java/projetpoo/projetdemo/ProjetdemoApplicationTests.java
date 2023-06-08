79582ae7df57aae1afb53584c01d8e61dc7b48b4
Electrobolt <115304849+Electrobolt@users.noreply.github.com>
2023-06-08 09:17:10 +0100

    edo_first-commit

---
+++ projetdemo/src/test/java/projetpoo/projetdemo/ProjetdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package projetpoo.projetdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
